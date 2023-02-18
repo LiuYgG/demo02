e46d0fa136479c2c4114d71b1c41e51af36627c5
liuyinggang <952904268@qq.com>
2023-02-18 20:09:42 +0800

    lastday

--- transport_demo/src/views/index/Index.vue
+++ transport_demo/src/views/index/Index.vue
@@ -41,7 +41,24 @@
             4. 城市导航
          -->
         <main>
-            中心内容
+            <!-- 详细分类 / banner轮播图 -->
+            <div class="wrapper">
+                <div class="sidebar">
+                    <ul :default-active="sidebarIndex">
+                        <li v-for="(item, index) in sidebars" :key="index"> {{ item }}</li>
+                    </ul>
+                </div>
+                <div class="content">
+                    <el-carousel :interval="5000" arrow="always">
+                        <el-carousel-item v-for="(item, index) in carousel" :key="index">
+                        <img :src="item.image" alt="">
+                        </el-carousel-item>
+                    </el-carousel>
+                </div>
+            </div>
+
+            <!-- 2.空白区域：待商榷 -->
+            <div class="nones"></div>
         </main>
 
         <!-- 
@@ -65,6 +82,17 @@
                 activeIndex: 0,
                 // menus的数据
                 menus:['首页', '全部工作', '关于我们'],
+
+                // main：左侧菜单
+                sidebarIndex: 0,
+                sidebars:['菜单一', '菜单二', '菜单三', '菜单四'],
+                // main: 轮播图
+                carousel: [
+                    { image: 'https://img1.baidu.com/it/u=95046244,3436270039&fm=253&fmt=auto&app=138&f=JPEG?w=843&h=500' },
+                    { image: 'https://img1.baidu.com/it/u=3180272130,671725760&fm=253&fmt=auto&app=120&f=JPEG?w=506&h=300' },
+                    { image: 'https://img2.baidu.com/it/u=181125032,1070588788&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=336' },
+                    { image: 'https://img0.baidu.com/it/u=3321194228,2911128357&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500' }
+                ]
             };
         },
     }
@@ -97,4 +125,63 @@
 .users{
     margin-right: 50px;
 }
+
+/**
+
+main内容专区
+
+*/
+.wrapper {
+    display: flex;
+    height: 300px;
+    width: 1200px;
+    justify-content: center;
+    margin: 10px auto;
+    overflow: hidden;
+}
+.sidebar {
+    width: 200px;
+    background-color: #f0f0f0;
+    border-right: 1px solid #ddd;
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+    padding: 20px;
+}
+.sidebar ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.sidebar li {
+    cursor: pointer;
+    padding: 10px;
+    margin-bottom: 10px;
+    border-radius: 4px;
+}
+li:hover {
+  background-color: #e6f7ff;
+}
+.content {
+    flex: 1;
+    height: 100%;
+    //   padding: 20px;
+    box-sizing: border-box;
+}
+.content .el-carousel{
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+}
+.content img{
+    width: 100%;
+    height: 300px;
+}
+
+// 2. 空白待商榷位置
+.nones{
+    width: 100%;
+    min-width: 1200px;
+    height: 60px;
+    background-color: #409EFF;
+}
+
 </style>
