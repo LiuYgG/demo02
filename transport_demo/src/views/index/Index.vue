363fc4f7b2afab00c906947c11d4c8506aa66759
liuyinggang <952904268@qq.com>
2023-02-18 17:35:01 +0800

    lastday

--- transport_demo/src/views/index/Index.vue
+++ transport_demo/src/views/index/Index.vue
@@ -6,20 +6,9 @@
             3. 菜单目录
             4. 用户中心
          -->
-        <div class="top-nav">
-            <div class="logo">
-                <h5>Logo</h5>
-            </div>
-            <div class="search">
-                <input type="text" name="" id="" placeholder="Search">
-                <button>Go</button>
-            </div>
-            <div class="menu">
-                <ul>
-                    <li v-for="item in menuItems" :key="item.id">{{ item.text }}</li>
-                </ul>
-            </div>
-        </div>
+        <header>
+            顶部内容
+        </header>
 
         <!-- 
             1. 详细分类 / banner轮播图
@@ -46,28 +35,8 @@
 
 <script>
     export default {
-        data(){
-            return {
-                menuItems:[
-                    { id: 1, text: 'Home' },
-                    { id: 2, text: 'About US' },
-                    { id: 3, text: 'Services' },
-                    { id: 4, text: 'Contact Us' },
-                ]
-            }
-        }
     }
 </script>
 
 <style lang="less" scoped>
-.top-nav {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 20px;
-    background-color: #fff;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    // width: 1200px;
-}
-
 </style>
