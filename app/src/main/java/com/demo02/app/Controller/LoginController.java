c3cb370422042ed8c11b445b842b65b250d6a068
liuyinggang <952904268@qq.com>
2023-03-08 22:58:22 +0800

    啦啦啦

--- app/src/main/java/com/demo02/app/Controller/LoginController.java
+++ app/src/main/java/com/demo02/app/Controller/LoginController.java
@@ -3,11 +3,12 @@ package com.demo02.app.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class LoginController {
 
     @GetMapping ("/login")
-    public String AdminUserLogin() {
+    public String UserLogin() {
         return "LoginPage";
     }
 }
